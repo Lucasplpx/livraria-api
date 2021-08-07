8a07d17c03ad5fe42d18dce93c095701d0b85bce
Lucas Passos <lucasplpx@gmail.com>
2021-08-06 21:02:19 -0300

    configurando dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { LivroModule } from './livro/livro.module';
-import { Livro } from './livro/model/livro.model';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'lucas',
+      username: process.env.USER_DB,
+      password: process.env.PASS_DB,
       database: 'livraria',
       autoLoadModels: true,
       synchronize: true,
